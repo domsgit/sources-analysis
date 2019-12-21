61c8c2f3390ab1bc1ede5d8d210244adfa394916
domsgit <domsgit@outlook.com>
2019-12-21 09:06:24 +0800

    add some fns to ramda/source/internal

---
+++ ramda/source/internal/_aperture.js
@@ -0,0 +1,14 @@
+// http://ramda.cn/docs/#aperture
+// 返回一个新列表，列表中的元素为由原列表相邻元素组成的 n 元组
+// R.aperture(2, [1, 2, 3, 4, 5]); //=> [[1, 2], [2, 3], [3, 4], [4, 5]]
+// R.aperture(3, [1, 2, 3, 4, 5]); //=> [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+export default function _aperture(n, list) {
+  var idx = 0;
+  var limit = list.length - (n - 1);
+  var acc = new Array(limit >= 0 ? limit : 0);
+  while (idx < limit) {
+    acc[idx] = Array.prototype.slice.call(list, idx, idx + n);
+    idx += 1;
+  }
+  return acc;
+}
---
+++ ramda/source/internal/_filter.js
@@ -0,0 +1,14 @@
+// 过滤函数，list中满足fn的才能通过
+export default function _filter(fn, list) {
+  var idx = 0;
+  var len = list.length;
+  var result = [];
+
+  while (idx < len) {
+    if (fn(list[idx])) {
+      result[result.length] = list[idx];
+    }
+    idx += 1;
+  }
+  return result;
+}

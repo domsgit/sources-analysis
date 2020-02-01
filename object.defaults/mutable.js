ca39eba96c95f310acc013f8c9b5f79c6a823ba1
domsgit <domsgit@outlook.com>
2020-02-01 23:06:26 +0800

    add object.defaults

---
+++ object.defaults/mutable.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var each = require('array-each');
+var slice = require('array-slice');
+var forOwn = require('for-own');
+var isObject = require('isobject');
+
+/**
+ * Extends the `target` object with properties of one or
+ * more additional `objects`
+ * 可选的 `objects` 一个或多个用于扩展 `target` 对象属性
+ *
+ * @name .defaults
+ * @param  {Object} `target` The target object. Pass an empty object to shallow clone. 目标对象。传递一个空对象以浅复制。
+ * @param  {Object} `objects`
+ * @return {Object}
+ * @api public
+ */
+
+module.exports = function defaults(target, objects) {
+  if (target == null) {
+    return {};
+  }
+
+  each(slice(arguments, 1), function(obj) {
+    if (isObject(obj)) {
+      forOwn(obj, function(val, key) {
+        if (target[key] == null) {
+          target[key] = val;
+        }
+      });
+    }
+  });
+
+  return target;
+};
